b577fbb18fa367d5f8f1214a2b4b39e3adddda0d
ProgramandoWeb2 <programacionwebcibertec@gmail.com>
2025-08-12 08:11:24 -0500

    initial project

---
+++ src/main/java/pe/edu/idat/demo_kafka_productor/DemoKafkaProductorApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_kafka_productor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKafkaProductorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoKafkaProductorApplication.class, args);
+	}
+
+}
